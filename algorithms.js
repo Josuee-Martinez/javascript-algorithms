c9736da25ec7624cf74f58b8a39c2a59da35411d
Josue Martinez <josuemartinez3141@gmail.com>
2022-01-31 22:12:20 -0500

    fcc

--- algorithms.js
+++ algorithms.js
@@ -4790,3 +4790,27 @@ function pairElement(str) {
 }
 
 // pairElement("ATCGA");
+
+function zeroArray(m, n) {
+   // Creates a 2-D array with m rows and n columns of zeroes
+   let newArray = [];
+   let row = [];
+   for (let i = 0; i < m; i++) {
+      // Adds the m-th row into newArray
+
+      for (let j = 0; j < n; j++) {
+         // Pushes n zeroes into the current row to create the columns
+
+         row.push(0);
+      }
+      newArray.push(row);
+      row = [];
+      // Pushes the current row, which now has n zeroes in it, to the array
+   }
+   return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+//[[0, 0], [0, 0], [0, 0]]
